3d6c2632b67f91aa932d294f19a5725abcf37645
DGclasher <73327721+DGclasher@users.noreply.github.com>
2023-03-24 09:35:51 +0530

    flasked

---
+++ app.py
@@ -0,0 +1,20 @@
+import os
+from flask import *
+
+app = Flask(__name__, static_url_path="/static", static_folder="./static")
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(port=port, debug=True)
