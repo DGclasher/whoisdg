3a24f1635bc90056dc94135c4ae7e7cb04e9152d
DGclasher <73327721+DGclasher@users.noreply.github.com>
2023-07-23 20:14:00 +0530

    updated

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 import os
+import secrets
+import smtplib
 from flask import *
 
 app = Flask(__name__, static_url_path="/static", static_folder="./static")
+app.secret_key = secrets.token_hex(16)
+
+def send_mail(email, passwd, message):
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+    server.login(email, passwd)
+    
 
 @app.route("/")
 def home():
